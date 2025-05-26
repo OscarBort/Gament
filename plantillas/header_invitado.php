40588bd5f0dd7072ab966b49e51959ed3f6fc79a
Oscar <obm_oscar@hotmail.com>
2025-05-26 15:55:50 +0200

    Funciona la búsqueda

--- plantillas/header_invitado.php
+++ plantillas/header_invitado.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="css/cssfooter.css">
     <link rel="stylesheet" href="css/cssjuegos.css">
     <link rel="stylesheet" href="css/cssregistro.css">
+    <link rel="stylesheet" href="css/cssbusqueda.css">
     <meta name="description" content="Una página copia de otras como Game o Gamestop">
     <meta name="author" content="Oscar Bort">
     <script src="https://kit.fontawesome.com/89d2629216.js" crossorigin="anonymous"></script>
@@ -35,13 +36,15 @@
         <div id=headerCentro>
             <div class="custom_input">
                 <svg xmlns="http://www.w3.org/2000/svg" class="svg_icon bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
-                <input class="input" type="text" placeholder="Busca tu juego">
+                <form id="busqueda" method="GET" action="busqueda.php">
+                <input class="input" type="text" name="busqueda" placeholder="Busca tu juego">
+                </form>
             </div>
         </div>
         <div id=headerDerecha>
             <button id="botonLogin" onclick="window.modal.showModal();">Login</button>
                 <dialog id="modal">
-                    <form method="POST" action="<?=($_SERVER['PHP_SELF'])?>">
+                    <form id="login" method="POST" action="<?=($_SERVER['PHP_SELF'])?>">
                         <label for="usuario" id="usuarioLabel">Usuario:</label><br>
                         <input type="text" id="usuarioLogin" name="usuario" autocomplete="off"><br>
                         <label for="password" id="passwordLabel">Contraseña</label><br>
--- plantillas/header_usuario.php
+++ plantillas/header_usuario.php
@@ -26,7 +26,9 @@
         <div id=headerCentro>
             <div class="custom_input">
                 <svg xmlns="http://www.w3.org/2000/svg" class="svg_icon bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path></svg>
-                <input class="input" type="text" placeholder="Busca tu juego">
+                <form id="busqueda" method="GET" action="busqueda.php">
+                <input class="input" type="text" name="busqueda" placeholder="Busca tu juego">
+                </form>
             </div>
         </div>
         <div id=headerDerecha>
