0768274782b9b8e124b70c0c1c9bee6b6df94fb4
Oscar <obm_oscar@hotmail.com>
2025-05-27 18:35:20 +0200

    registro funcional

--- plantillas/contenido_registro.php
+++ plantillas/contenido_registro.php
@@ -46,3 +46,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </label>
     <button class="submit">Submit</button>
 </form>
+<?php
+$usuario = isset($_POST['usuario']) ? $_POST['usuario'] : '';
+$passwordR = isset($_POST['password']) ? password_hash($_POST['password'], PASSWORD_DEFAULT) : '';
+$email = isset($_POST['email']) ? $_POST['email'] : '';
+$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+$apellido1 = isset($_POST['apellido1']) ? $_POST['apellido1'] : '';
+$apellido2 = isset($_POST['apellido2']) ? $_POST['apellido2'] : '';
+$NIF = isset($_POST['NIF']) ? $_POST['NIF'] : '';
+$fnacimiento = isset($_POST['fnacimiento']) ? $_POST['fnacimiento'] : '';
+$telefono = isset($_POST['telefono']) ? $_POST['telefono'] : '';
+try {
+            $conn = db_connect(); // ← Tu función que devuelve un objeto PDO
+            $sql = "INSERT INTO usuarios (usuario, password, email, nombre, apellido1, apellido2, NIF, fnacimiento, telefono)
+                    VALUES ($usuario, $passwordR, $email, $nombre, $apellido1, $apellido2, $NIF, $fnacimiento, $telefono)";
+            $conn->exec($sql);
+        } catch (PDOException $e) {
+            echo "Error al guardar en la base de datos: " . $e->getMessage();
+            exit;
+        }
+?>
