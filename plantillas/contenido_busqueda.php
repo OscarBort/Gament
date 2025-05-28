fad2e605a7b49a9da9336fb5749d8210b8422325
Oscar <obm_oscar@hotmail.com>
2025-05-28 18:55:45 +0200

    Pagina busqueda con enlaces

--- plantillas/contenido_busqueda.php
+++ plantillas/contenido_busqueda.php
@@ -5,7 +5,7 @@ if (isset($_GET["busqueda"])) {
     try {
         $conn = db_connect();
 
-        $sql = "SELECT juegos.id_juego, portada, juegos.nombre AS nombreJ, descripcion, precio, genero FROM juegos INNER JOIN compañias ON juegos.id_compañia=compañias.id_compañia WHERE juegos.nombre LIKE '%" . $_GET['busqueda'] . "%' OR compañias.nombre='" . $_GET['busqueda'] . "' OR juegos.genero='" . $_GET['busqueda'] . "' GROUP BY juegos.nombre;";
+        $sql = "SELECT juegos.id_juego AS ID, portada, juegos.nombre AS nombreJ, descripcion, precio, genero FROM juegos INNER JOIN compañias ON juegos.id_compañia=compañias.id_compañia WHERE juegos.nombre LIKE '%" . $_GET['busqueda'] . "%' OR compañias.nombre='" . $_GET['busqueda'] . "' OR juegos.genero='" . $_GET['busqueda'] . "' GROUP BY juegos.nombre;";
         $results = db_query($conn, $sql);
     } catch(Exception $e) {
         echo $e->getMessage();
@@ -16,24 +16,24 @@ if (isset($_GET["busqueda"])) {
         echo "<div class='resultadosBusquedaWrapper'>";
         
         foreach ($results as $result) {
-            echo "<div class='contenedorBusqueda'>";
-                echo "<div class='portadaBusqueda'>";
-                    echo "<img src='" . htmlspecialchars($result['portada']) . "'>";
-                    echo "<p>" . htmlspecialchars($result['nombreJ']) . "</p>";
-                echo "</div>";
-                echo "<div class='datosBusqueda'>";
-                    echo "<div class='textoDescripcion'>" . htmlspecialchars($result['descripcion']) . "</div>";
-                    echo "<div class='precioCarrito'>";
-                    echo "<i class='fas fa-shopping-cart' style='cursor:pointer;' onclick='agregarAlCarrito(" .
-                        '"' . addslashes($result['nombreJ']) . '",' .
-                        '"' . $result['precio'] . '",' .
-                        '"' . $result['portada'] . '"' .
-                        ")'></i> ";
-                    echo "<span>" . htmlspecialchars($result['precio']) . "€</span></div>";
-
-                echo "</div>";
-            echo "</div>";
-
+            echo "<a id='enlaceBusqueda' href='pagina_juegos.php?id=" . htmlspecialchars($result['ID']) . "'>";
+                echo "<div class='contenedorBusqueda'>";
+                    echo "<div class='portadaBusqueda'>";
+                        echo "<img src='" . htmlspecialchars($result['portada']) . "'>";
+                        echo "<p>" . htmlspecialchars($result['nombreJ']) . "</p>";
+                    echo "</div>";
+                    echo "<div class='datosBusqueda'>";
+                        echo "<div class='textoDescripcion'>" . htmlspecialchars($result['descripcion']) . "</div>";
+                        echo "<div class='precioCarrito'>";
+                            echo "<button id='botonBusqueda' class='fas fa-shopping-cart' style='cursor:pointer;' onclick='event.preventDefault(); event.stopPropagation(); agregarAlCarrito(" .
+                                '"' . addslashes($result['nombreJ']) . '",' .
+                                '"' . $result['precio'] . '",' .
+                                '"' . $result['portada'] . '"' .
+                                ")'aria-label='Añadir al carrito'></button>";
+                            echo "<span>" . htmlspecialchars($result['precio']) . "€</span>";
+                        echo "</div>";
+                    echo "</div>";
+                echo "</div></a>";
         }
 
         echo "</div>"; // Cierra resultadosBusquedaWrapper
